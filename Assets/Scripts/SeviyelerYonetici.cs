ecfd54bb831cc7ddd2d7137200cf03a92b592c66
Anil Yavuz <anielyavuz@gmail.com>
2023-08-19 23:56:08 +0300

    23:57 19.08.2023

--- Assets/Scripts/SeviyelerYonetici.cs
+++ Assets/Scripts/SeviyelerYonetici.cs
@@ -8,10 +8,6 @@ public class SeviyelerYonetici : MonoBehaviour
     // Start is called before the first frame update
     public Button[] levelButtons;
     public int amountOfLevelButtons = 6;
-    public static bool seviye1, seviye2, seviye3, seviye4, seviye5, seviye6;
-    public Button seviye1_button, seviye2_button, seviye3_button, seviye4_button, seviye5_button, seviye6_button;
-
-
     public static bool yeniLevelInfo=false;
     public static int yeniLevelID=1;
     private void Awake()
@@ -30,7 +26,7 @@ public class SeviyelerYonetici : MonoBehaviour
 
     private void Start()
     {
-        seviye1 = true;
+     levelButtons[0].interactable =true;
     }
     private void Update()
     {
@@ -48,27 +44,5 @@ public class SeviyelerYonetici : MonoBehaviour
         }
 
 
-        // if (seviye2 == true)
-        // {
-        //     seviye2_button.interactable = true;
-        // }
-        // if (seviye3 == true)
-        // {
-        //     seviye3_button.interactable = true;
-        // }
-        // if (seviye4 == true)
-        // {
-        //     seviye4_button.interactable = true;
-        //     //
-        // }
-
-        // if (seviye5 == true)
-        // {
-        //     seviye5_button.interactable = true;
-        // }
-        // if (seviye6 == true)
-        // {
-        //     seviye6_button.interactable = true;
-        // }
     }
 }
